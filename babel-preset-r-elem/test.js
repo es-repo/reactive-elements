64ec99ea392c41818444a12ef0c1f15b9bf7ee62
Eugene Sirota <ES-Macbook@es-mbr.local>
2019-11-17 07:46:09 +0200

    Update test

--- babel-preset-r-elem/test.js
+++ babel-preset-r-elem/test.js
@@ -2,7 +2,7 @@ const babel = require('@babel/core');
 const preset = require('.');
 const assert = require('assert');
 
-const { code } = babel.transformSync('const v = <div b={2} />;', {
+const { code } = babel.transformSync('const v = <div a b={2} />;', {
 	presets: [
 		preset
 	],
@@ -10,5 +10,5 @@ const { code } = babel.transformSync('const v = <div b={2} />;', {
 	compact: true
 });
 
-assert.equal(code, 'const v=elem("div").set(e=>{e.b=2;});');
+assert.equal(code, 'const v=elem("div").set(e=>{e.a=true;e.b=2;});');
 console.log('passed');
